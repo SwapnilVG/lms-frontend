b92e0b478de392a75580a5c147ee2bc7531f0da9
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-03-15 15:30:41 +0530

    configures auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    Swapnil Gadekar
+      <Routes>
+
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// Component imports
 import App from './App.jsx'
+// CSS imports
 import './index.css'
 
+// Library imports
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
  
 )
