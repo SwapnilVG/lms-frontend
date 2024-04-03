7491d21c77132fd2d2037149ea5b6369de115420
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-04-03 16:43:00 +0530

    Cleaned up our AboutUs Page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,11 +1,46 @@
-import HomeLayout from "../Layouts/HomeLayout"
-import aboutMainImage from '../Assets/Images/aboutMainImage.png'
-import apj from '../Assets/Images/apj.png'
-import billGates from '../Assets/Images/billGates.png'
-import einstein from '../Assets/Images/einstein.png'
-import nelsonMandela from '../Assets/Images/nelsonMandela.png'
-import steveJobs from '../Assets/Images/steveJobs.png'
+import HomeLayout from "../Layouts/HomeLayout";
+import aboutMainImage from '../Assets/Images/aboutMainImage.png';
+import apj from '../Assets/Images/apj.png';
+import billGates from '../Assets/Images/billGates.png';
+import einstein from '../Assets/Images/einstein.png';
+import nelsonMandela from '../Assets/Images/nelsonMandela.png';
+import steveJobs from '../Assets/Images/steveJobs.png';
+import CarouselSlide from "../Components/CarouselSlide";
+
 function AboutUs(){
+
+    const celebrities = [
+        {
+            title: "Nelson Mandela",
+            description:"Education is the most powerful tool you can use to change the world.",
+            image:nelsonMandela,
+            slideNumber:1
+        },
+        {
+            title: "APJ Abdul Kalam",
+            description:"Failure will never overtake me if my determination to succeed is strong enough.",
+            image:apj,
+            slideNumber:2
+        },
+        {
+            title: "Albert Einstein",
+            description:"A person who never made a mistake never tried anything new.",
+            image:einstein,
+            slideNumber:3
+        },
+        {
+            title: "Steve Jobs",
+            description:"We don't get a chance to do that many things, and every one should be really excellent.",
+            image:steveJobs,
+            slideNumber:4
+        },
+        {
+            title: "Bill Gates",
+            description:"Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+            image:billGates,
+            slideNumber:5
+        },
+]
     return(
         <HomeLayout>
             <div className=" pl-20 pt-20 flex flex-col text-white">
@@ -33,77 +68,11 @@ function AboutUs(){
                     </div>
                 </div>
 
-                <div className="carousel w-1/2 my-16 m-auto">
-                    
-                        <div id="slide1" className="carousel-item relative w-full">
-                            <div className=" flex flex-col items-center justify-center gap-4 px-[15%]">
-                                <img src={nelsonMandela} className=" w-40 rounded-full border-2 border-gray-400" />
-                                <p className=" text-xl text-gray-200">
-                                    {"Education is the most powerful tool you can use to change the world."}
-                                </p>
-                                <h3 className=" text-2xl font-bold">Nelson Mandela</h3>
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide5" className="btn btn-circle">❮</a> 
-                                    <a href="#slide2" className="btn btn-circle">❯</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div id="slide2" className="carousel-item relative w-full">
-                            <div className=" flex flex-col items-center justify-center gap-4 px-[15%]">
-                                <img src={apj} className=" w-40 rounded-full border-2 border-gray-400" />
-                                <p className=" text-xl text-gray-200">
-                                    {"Failure will never overtake me if my determination to succeed is strong enough."}
-                                </p>
-                                <h3 className=" text-2xl font-bold">APJ Abdul Kalam</h3>
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide1" className="btn btn-circle">❮</a> 
-                                    <a href="#slide3" className="btn btn-circle">❯</a>
-                                </div>
-                            </div>
-                        </div> 
-
-                        <div id="slide3" className="carousel-item relative w-full">
-                            <div className=" flex flex-col items-center justify-center gap-4 px-[15%]">
-                                <img src={einstein} className=" w-40 rounded-full border-2 border-gray-400" />
-                                <p className=" text-xl text-gray-200">
-                                    {"A person who never made a mistake never tried anything new."}
-                                </p>
-                                <h3 className=" text-2xl font-bold">Albert Einstein</h3>
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide2" className="btn btn-circle">❮</a> 
-                                    <a href="#slide4" className="btn btn-circle">❯</a>
-                                </div>
-                            </div>
-                        </div>
-                         
-                        <div id="slide4" className="carousel-item relative w-full">
-                            <div className=" flex flex-col items-center justify-center gap-4 px-[15%]">
-                                <img src={steveJobs} className=" w-40 rounded-full border-2 border-gray-400" />
-                                <p className=" text-xl text-gray-200">
-                                    {"We don't get a chance to do that many things, and every one should be really excellent."}
-                                </p>
-                                <h3 className=" text-2xl font-bold">Steve Jobs</h3>
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide3" className="btn btn-circle">❮</a> 
-                                    <a href="#slide5" className="btn btn-circle">❯</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div id="slide5" className="carousel-item relative w-full">
-                            <div className=" flex flex-col items-center justify-center gap-4 px-[15%]">
-                                <img src={billGates} className=" w-40 rounded-full border-2 border-gray-400" />
-                                <p className=" text-xl text-gray-200">
-                                    {"Success is a lousy teacher. It seduces smart people into thinking they can't lose."}
-                                </p>
-                                <h3 className=" text-2xl font-bold">Bill Gates</h3>
-                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide4" className="btn btn-circle">❮</a> 
-                                    <a href="#slide1" className="btn btn-circle">❯</a>
-                                </div>
-                            </div>
-                        </div>
+                <div className=" carousel w-1/2 my-16 m-auto">
+                        {celebrities && celebrities.map(celebrity => (<CarouselSlide 
+                        {...celebrity} 
+                        key={celebrity.slideNumber} 
+                        totalSlides = {celebrities.length} />))}
                </div>
             </div>
         </HomeLayout>
@@ -111,4 +80,4 @@ function AboutUs(){
 }
 
 
-export default AboutUs
+export default AboutUs;
