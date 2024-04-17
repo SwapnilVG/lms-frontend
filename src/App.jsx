8b159baa552fc1de139998b2a524c1b6e1dd616f
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-04-17 12:06:36 +0530

    Prepared course details component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Pages/Login'
 import CourseList from './Pages/Course/CourseList'
 import Contact from './Pages/Contact'
 import Denied from './Pages/Denied'
+import CourseDescription from './Pages/Course/CourseDescription'
 
 
 
@@ -21,6 +22,7 @@ function App() {
         <Route path='/courses' element={<CourseList/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/denied' element={<Denied/>}></Route>
+        <Route path='/course/description' element = {<CourseDescription/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='*' element={<Notfound/>}></Route>
