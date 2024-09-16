4b13020ada90185fa3f4de18cae4e517176d14a9
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-09-17 00:09:17 +0530

    add vercel.json

--- src/Helpers/axiosinstance.js
+++ src/Helpers/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://backend-project-guqr.onrender.com/api/v1"
+const BASE_URL = "https://backend-project-32b2.onrender.com/api/v1"  
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
